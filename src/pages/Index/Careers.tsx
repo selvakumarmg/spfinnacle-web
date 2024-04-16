4f6d0bcb9ab318f430a42b4e37bd9e38c0806aef
AshAk0 <webdeveloperashak@gmail.com>
2024-04-16 15:32:54 +0530

    Created Upload Resume with restrictions and functionality

--- src/pages/Index/Careers.tsx
+++ src/pages/Index/Careers.tsx
@@ -1,11 +1,21 @@
 import RightArrowIcon from "assets/images/RightArrowIcon";
 import { Text } from "components";
-import React from "react";
+import React, { useState } from "react";
+import ResumeUpload from "./ResumeUpload";
 
 export default function Careers() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
   return (
     <>
-      <div className="w-[80%] h-fit mx-auto md:grid-cols-1 lg:grid-cols-1 grid grid-cols-2 gap-y-5 place-items-center">
+      <div className="relative w-[80%] h-fit mx-auto md:grid-cols-1 lg:grid-cols-1 grid grid-cols-2 gap-y-5 place-items-center">
         {/* Career 1 */}
         <div className="w-[500px] h-[230px] border-2 border-[#E80D55] rounded-md flex flex-col gap-y-4 justify-center p-8">
           <Text as="h2" className="text-[#E80D55] !font-semibold !text-xl">
@@ -16,12 +26,13 @@ export default function Careers() {
             optio aut tempora natus. 33 cupiditate quis et odit dignissimos et
             quaerat amet ut recusandae consequatur est.
           </Text>
-          <Text
-            as="p"
-            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3"
+          <button
+            type="button"
+            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3 hover:cursor-pointer"
+            onClick={openDialog}
           >
             Apply Now <RightArrowIcon className="w-5 text[#0F528B]" />
-          </Text>
+          </button>
         </div>
         {/* Career 1 */}
         {/* Career 2 */}
@@ -34,12 +45,13 @@ export default function Careers() {
             optio aut tempora natus. 33 cupiditate quis et odit dignissimos et
             quaerat amet ut recusandae consequatur est.
           </Text>
-          <Text
-            as="p"
-            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3"
+          <button
+            type="button"
+            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3 hover:cursor-pointer"
+            onClick={openDialog}
           >
             Apply Now <RightArrowIcon className="w-5 text[#0F528B]" />
-          </Text>
+          </button>
         </div>
         {/* Career 2 */}
         {/* Career 3 */}
@@ -52,12 +64,13 @@ export default function Careers() {
             optio aut tempora natus. 33 cupiditate quis et odit dignissimos et
             quaerat amet ut recusandae consequatur est.
           </Text>
-          <Text
-            as="p"
-            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3"
+          <button
+            type="button"
+            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3 hover:cursor-pointer"
+            onClick={openDialog}
           >
             Apply Now <RightArrowIcon className="w-5 text[#0F528B]" />
-          </Text>
+          </button>
         </div>
         {/* Career 3 */}
         {/* Career 4 */}
@@ -70,15 +83,22 @@ export default function Careers() {
             optio aut tempora natus. 33 cupiditate quis et odit dignissimos et
             quaerat amet ut recusandae consequatur est.
           </Text>
-          <Text
-            as="p"
-            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3"
+          <button
+            type="button"
+            className="!font-medium !text-lg text-[#0F528B] flex gap-x-3 hover:cursor-pointer"
+            onClick={openDialog}
           >
             Apply Now <RightArrowIcon className="w-5 text[#0F528B]" />
-          </Text>
+          </button>
         </div>
         {/* Career 4 */}
+        {isOpen && (
+          <div className="absolute w-full h-fit top-[-25%]">
+            <ResumeUpload />
+          </div>
+        )}
       </div>
+      {/* <FileUploadForm /> */}
     </>
   );
 }
--- src/pages/Index/ScrollToTheTop.tsx
+++ src/pages/Index/ScrollToTheTop.tsx
@@ -35,10 +35,22 @@ const ScrollToTopButton: React.FC = () => {
       {isVisible && (
         <button
           title="scroll-to-the-top"
-          className="fixed bottom-4 right-4 bg-blue-500 text-white p-3 rounded-full shadow-md"
+          className="fixed bottom-4 right-4 bg-[#E80D55] text-white p-3 rounded-full shadow-md"
           onClick={scrollToTop}
         >
           <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+            width="40"
+            height="40"
+            className="w-10 h-10"
+          >
+            <path
+              fill="#FFFFFF"
+              d="M318 177.5c3.8-8.8 2-19-4.6-26l-136-144C172.9 2.7 166.6 0 160 0s-12.9 2.7-17.4 7.5l-136 144c-6.6 7-8.4 17.2-4.6 26S14.4 192 24 192H96l0 288c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32l0-288h72c9.6 0 18.2-5.7 22-14.5z"
+            />
+          </svg>
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             width="24"
@@ -47,7 +59,7 @@ const ScrollToTopButton: React.FC = () => {
           >
             <path fill="none" d="M0 0h24v24H0z" />
             <path d="M12 2L1 13h6v9h10v-9h6z" />
-          </svg>
+          </svg> */}
         </button>
       )}
     </div>
